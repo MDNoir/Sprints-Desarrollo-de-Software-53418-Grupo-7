8b07074340382519baf05a24a583207dd031d8a7
Mario Ojeda-Cuello <3382418+MDNoir@users.noreply.github.com>
2021-09-24 19:22:03 -0500

    validaciones.js completado funcionalmente

--- validaciones.js
+++ validaciones.js
@@ -1,25 +1,49 @@
 function validar_nombre_usuario(string) {
     var x = document.getElementById("dato_nombre_usuario").value;
-    var re = new RegExp("^([A-Z]{1,}[a-z]*?)(\\s[A-Z]{1,}[a-z]*?)?(\\s[A-Z]{1,}[a-z]*?)?(\\s[A-Z]{1,}[a-z]*?)?$");
-    if (re.test(x) == true) {
-        window.alert("Validación Completada");
+    var re_nom = new RegExp("^([A-Z]{1,}[a-z]*?)(\\s[A-Z]{1,}[a-z]*?)?(\\s[A-Z]{1,}[a-z]*?)?(\\s[A-Z]{1,}[a-z]*?)?$");
+    if (re_nom.test(x) == true) {
+        window.alert("Validación Nombre Completada");
         return true;
     }
     else {
-        window.alert("Datos no válidos")
+        window.alert("Nombre no válido")
         return false;
     }
 }
 
 function validar_edad_usuario(edad) {
-    var y = document.getElementById("dato_edad_usuario").value;
+    var y = parseInt(document.getElementById("dato_edad_usuario").value);
+    if(typeof(y) == 'number'){
+        if (y >= 13 && y < 110 ) {
+            window.alert("Validación Edad Completada");
+            return true;
+        }
+        else {
+            window.alert("Edad no válida")
+            return false;
+        }
+    }
+    else{
+        window.alert("Por favor escriba un numero")
+        return false;
+    }
+    
 }
 
 function validar_contrasena(string) {
     var z = document.getElementById("dato_contrasena").value;
+    var re_con = new RegExp("^[a-zA-Z0-9]{6,}$");
+    if (re_con.test(z) == true) {
+        window.alert("Validación Contraseña Completada");
+        return true;
+    }
+    else {
+        window.alert("Contraseña no válida")
+        return false;
+    }
 }
 
-/*
+
 module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_edad_usuario = validar_edad_usuario;
-module.exports.validar_contrasena = validar_contrasena;*/
+module.exports.validar_contrasena = validar_contrasena;
