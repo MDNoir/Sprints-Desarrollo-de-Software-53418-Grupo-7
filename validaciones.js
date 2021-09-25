5f2cedaf12a75fdc537eed4aa67b101b37baba93
Mario Ojeda-Cuello <3382418+MDNoir@users.noreply.github.com>
2021-09-24 19:06:35 -0500

    RegEx val nom y js probado

--- validaciones.js
+++ validaciones.js
@@ -1,8 +1,8 @@
 function validar_nombre_usuario(string) {
-    var x = document.getElementById("dato_edad_usuario").value;
-    var re = new RegExp("^([a-zA-Z]{1,}\s?([a-zA-Z]{1,})?\s?([a-zA-Z]{1,})?\s?([a-zA-Z]{1,})?\s?([a-zA-Z]{1,})?\s?");
+    var x = document.getElementById("dato_nombre_usuario").value;
+    var re = new RegExp("^([A-Z]{1,}[a-z]*?)(\\s[A-Z]{1,}[a-z]*?)?(\\s[A-Z]{1,}[a-z]*?)?(\\s[A-Z]{1,}[a-z]*?)?$");
     if (re.test(x) == true) {
-        window.alert("Validación Completada")
+        window.alert("Validación Completada");
         return true;
     }
     else {
@@ -19,6 +19,7 @@ function validar_contrasena(string) {
     var z = document.getElementById("dato_contrasena").value;
 }
 
+/*
 module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_edad_usuario = validar_edad_usuario;
-module.exports.validar_contrasena = validar_contrasena;
+module.exports.validar_contrasena = validar_contrasena;*/
