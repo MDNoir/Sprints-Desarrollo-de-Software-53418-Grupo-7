2b0384e6fd0b92fc3e254cdedb0c6e6f7efffb82
Mario Ojeda-Cuello <3382418+MDNoir@users.noreply.github.com>
2021-10-12 18:52:33 -0500

    archivos login

--- login.js
+++ login.js
@@ -1,7 +1,33 @@
-var re_nom = new RegExp("^[Bb][Oo][Go][Tt][AÁaá]$");
-if (re_nom.test(x) == true) {
-    return true;
+let registros = [];
+
+
+function validar_captcha(recaptcha){
+    var x = document.getElementById("in_login_captcha").value;
+    var re_nom = new RegExp("^[Bb][Oo][Go][Tt][AÁaá]$");
+    if (re_nom.test(x) == true) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-else {
-    return false;
+
+
+function iniciar_sesion(usuario, contrasena, recaptcha){
+    
+    var y = document.getElementById("in_login_username").value;
+    for (var i = 0; i < registros.length; i++) {
+        if (y == registros[1][us]){
+            return true;
+        }
+        else{
+            return false; 
+        }
+    }
 }
+
+module.exports.registros = registros;
+module.exports.validar_captcha = validar_captcha;
+module.exports.iniciar_sesion = iniciar_sesion;
+
+
