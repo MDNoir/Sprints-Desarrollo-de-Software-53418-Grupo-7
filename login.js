a90382e8827ea6fffc744bdd1550857472f92573
Mario Ojeda-Cuello <3382418+MDNoir@users.noreply.github.com>
2021-10-12 19:23:38 -0500

    Entregable v1

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 let registros = [];
 
-// prueba
+/*
 
 var reg = { 
     usuario: "mdoc",
@@ -9,13 +9,12 @@ var reg = {
 }
 
 registros.push(reg)
-
+*/
 
 function validar_captcha(recaptcha){
     var x = document.getElementById("in_login_captcha").value;
-    var re_cap = new RegExp("^[Bb][Oo][Go][Tt][AÁaá]$");
+    var re_cap = new RegExp("^[Bb][Oo][Gg][Oo][Tt][AÁaá]$");
     if (re_cap.test(x) == true) {
-        console.log("eres humano")
         return true;
     }
     else {
@@ -28,9 +27,9 @@ function iniciar_sesion(user, contra, recaptcha){
     
     var y = document.getElementById("in_login_username").value;
     var z = document.getElementById("in_login_contrasena").value;
+    var w = validar_captcha();
     for (var i = 0; i < registros.length; i++) {
-        if (y == registros[i][usuario] && z == registros[i][contrasena]){
-            console.log("validado")
+        if (y == registros[i].usuario && z == registros[i].contrasena){
             return true;
         }
         else{
@@ -39,13 +38,15 @@ function iniciar_sesion(user, contra, recaptcha){
     }
 }
 
+/*
 function test(){
     validar_captcha();
-    //iniciar_sesion();
+    iniciar_sesion();
 }
-/*
+*/
+
 module.exports.registros = registros;
 module.exports.validar_captcha = validar_captcha;
 module.exports.iniciar_sesion = iniciar_sesion;
-*/
+
 
