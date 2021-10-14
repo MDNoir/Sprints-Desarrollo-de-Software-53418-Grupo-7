72745c64f817c2975e2d33536c16340a94af11bf
Mario Ojeda-Cuello <3382418+MDNoir@users.noreply.github.com>
2021-10-13 20:54:38 -0500

    express cambio dir

--- proyecto_mintic/myfirst.js
+++ proyecto_mintic/myfirst.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+app.use(express.static('public'));
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
