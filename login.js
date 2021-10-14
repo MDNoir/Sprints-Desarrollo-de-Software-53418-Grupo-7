7780c7a77ecf8311be474d46d7dd7c136eaffe57
Mario Ojeda-Cuello <3382418+MDNoir@users.noreply.github.com>
2021-10-14 18:25:41 -0500

    Correcciones menores.

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ registros.push(reg)
 */
 
 function validar_captcha(recaptcha){
-    var x = document.getElementById("in_login_captcha").value;
+    var x = document.getElementById(recaptcha).value;
     var re_cap = new RegExp("^[Bb][Oo][Gg][Oo][Tt][AÁaá]$");
     if (re_cap.test(x) == true) {
         return true;
@@ -22,21 +22,23 @@ function validar_captcha(recaptcha){
     }
 }
 
-
-function iniciar_sesion(user, contra, recaptcha){
+function iniciar_sesion(user, contra, recap){
     
-    var y = document.getElementById("in_login_username").value;
-    var z = document.getElementById("in_login_contrasena").value;
-    var w = validar_captcha();
+    var y = document.getElementById(user).value;
+    var z = document.getElementById(contra).value;
+    var w = validar_captcha(recap);
     for (var i = 0; i < registros.length; i++) {
         if (y == registros[i].usuario && z == registros[i].contrasena && w == true){
+            console.log("sesion iniciada");
             return true;
         }
         else{
+            console.log("error");
             return false; 
         }
     }
 }
+
 
 /*
 function test(){
